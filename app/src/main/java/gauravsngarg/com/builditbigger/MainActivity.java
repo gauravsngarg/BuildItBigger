503373d08028285752bb69853c2bf2a5616b8476
Gaurav Garg <gauravsngarg@gmail.com>
2018-10-07 13:47:20 +0530

    Fetch Joke from Java Library

--- app/src/main/java/gauravsngarg/com/builditbigger/MainActivity.java
+++ app/src/main/java/gauravsngarg/com/builditbigger/MainActivity.java
@@ -2,12 +2,23 @@ package gauravsngarg.com.builditbigger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import gauravsngarg.com.javajokes.Joker;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+
+        Joker joker = new Joker();
+        mTextView.setText(joker.getJoke());
+
+
     }
 }
