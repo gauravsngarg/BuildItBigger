efcb3dcbf257196b3bfb963ec808fdeb9e4bb3b4
Gaurav Garg <gauravsngarg@gmail.com>
2018-10-07 13:59:16 +0530

    On Click: Show Toast

--- app/src/main/java/gauravsngarg/com/builditbigger/MainActivity.java
+++ app/src/main/java/gauravsngarg/com/builditbigger/MainActivity.java
@@ -1,24 +1,30 @@
 package gauravsngarg.com.builditbigger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import gauravsngarg.com.javajokes.Joker;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView mTextView;
+    Button btShowJoke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.textView);
+        btShowJoke = (Button) findViewById(R.id.btnShowJoke);
 
-        Joker joker = new Joker();
-        mTextView.setText(joker.getJoke());
-
-
+        final Joker joker = new Joker();
+        btShowJoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, joker.getJoke(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
